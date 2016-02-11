78c58cbab5f2908c1bcf76d2834bb27e44525e42
elvirafortune <edzhurae@cisco.com>
2016-02-10 18:41:42 -0800

    routing for pages

--- public/app.js
+++ public/app.js
@@ -1,16 +1,36 @@
 'use strict';
 
-angular.module('myApp', [
-  'ui.router'
-]).config(function($stateProvider, $urlRouterProvider) {
+var app = angular.module('myApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/');
     
-    $stateProvider.state('app', {
+    $stateProvider.state('main', {
         url: '/',
-        templateUrl: 'app/app.html',
-        controller: 'AppCtrl'
-    });
+        templateUrl: 'templates/main.html',
+        controller: 'MainCtrl'
+    })
+    .state('about', {
+        url: '/about',
+        templateUrl: 'templates/about.html',
+        controller: 'AboutCtrl'
+    })
+    .state('skills', {
+        url: '/skills',
+        templateUrl: 'templates/skills.html',
+        controller: 'SkillsCtrl'
+    })
+    .state('experience', {
+        url: '/experience',
+        templateUrl: 'templates/experience.html',
+        controller: 'ExperienceCtrl'
+    })
+    .state('contact', {
+        url: '/contact',
+        templateUrl: 'templates/contact.html',
+        controller: 'ContactCtrl'
+    })
     
 }).run(function () {
 
